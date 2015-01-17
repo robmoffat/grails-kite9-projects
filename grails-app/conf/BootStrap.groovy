f1357548d1fc3ec9d04e125046d2779062c31ee8
Rob Moffat <robmoffat@mac.com>
2015-01-17 16:55:22 +0000

    Added email field to user

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,7 +8,7 @@ class BootStrap {
         def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
         def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
 
-        def testUser = new User(username: 'admin', enabled: true, password: 'admin')
+        def testUser = new User(username: 'admin', enabled: true, password: 'admin', email: 'robmoffat@mac.com')
         testUser.save(flush: true)
 
         UserRole.create testUser, adminRole, true
